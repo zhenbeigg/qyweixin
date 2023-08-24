0ec5da248078dbd8623442151e29d88a39e6cfec
bool <zhenbeigg@163.com>
2023-08-24 19:36:11 +0800

    更新验签类

--- src/CallbackVerify.php
+++ src/CallbackVerify.php
@@ -3,7 +3,7 @@
  * @author: 布尔
  * @name: 企业微信回调验签
  * @desc: 介绍
- * @LastEditTime: 2023-08-24 15:30:19
+ * @LastEditTime: 2023-08-24 19:03:11
  */
 namespace Eykj\Qyweixin;
 
@@ -19,13 +19,20 @@ class CallbackVerify
     public function verify(array $param)
     {
         $wxcpt = new WXBizMsgCrypt($param['token'], $param['encodingAesKey'], $param['receiveid']);
-        /* 验签 */
         $echo_str = '';
-        $err_code = $wxcpt->VerifyURL($param['msg_signature'],$param['timestamp'],$param['nonce'],$param['echostr'],$echo_str);
+        if(isset($param['data'])){
+            /* 检验消息的真实性，并且获取解密后的明文 */
+            $err_code = $wxcpt->DecryptMsg($param['msg_signature'], $param['timestamp'],$param['nonce'],$param['data'], $echo_str);
+        }else{
+            /* url验签 */
+            $err_code = $wxcpt->VerifyURL($param['msg_signature'],$param['timestamp'],$param['nonce'],$param['echostr'],$echo_str);
+        }
         if ($err_code == 0) {
             return $echo_str;
         } else {
             return $err_code;
         }
     }
+
+    
 }
