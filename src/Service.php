e38ba37a7f10704a2358be58e6be5543f5ae6131
bool <zhenbeigg@163.com>
2023-08-28 17:27:18 +0800

    修改：service

--- src/Service.php
+++ src/Service.php
@@ -5,6 +5,7 @@
  * @desc: 介绍
  * @LastEditTime: 2023-08-18 18:07:52
  */
+
 namespace Eykj\Qyweixin;
 
 use Eykj\Base\GuzzleHttp;
@@ -18,7 +19,7 @@ class Service
     protected ?AuthInterface $AuthInterface;
 
     // 通过设置参数为 nullable，表明该参数为一个可选参数
-    public function __construct(?GuzzleHttp $GuzzleHttp,?AuthInterface $AuthInterface)
+    public function __construct(?GuzzleHttp $GuzzleHttp, ?AuthInterface $AuthInterface)
     {
         $this->GuzzleHttp = $GuzzleHttp;
         $this->AuthInterface = $AuthInterface;
@@ -101,7 +102,23 @@ class Service
      */
     public function get_permanent_code(array $param): array
     {
-        $r = $this->GuzzleHttp->get(env('QYWEIXIN_URL', 'https://qyapi.weixin.qq.com') . '/cgi-bin/service/get_permanent_code?suite_access_token=' . $this->get_suite_token($param), eyc_array_key($param, 'auth_code'));
+        $r = $this->GuzzleHttp->get(env('QYWEIXIN_URL', 'https://qyapi.weixin.qq.com') . '/cgi-bin/service/get_permanent_code?suite_access_token=' . $this->get_suite_token($param), eyc_array_key($param, 'auth_code|AuthCode,auth_code'));
+        if ($r["errcode"] == 0) {
+            return $r;
+        } else {
+            error(500, $r['errmsg']);
+        }
+    }
+
+    /**
+     * @author: 布尔
+     * @name: 获取企业授权信息
+     * @param array $param
+     * @return array
+     */
+    public function get_auth_info(array $param): array
+    {
+        $r = $this->GuzzleHttp->get(env('QYWEIXIN_URL', 'https://qyapi.weixin.qq.com') . '/cgi-bin/service/get_auth_info?suite_access_token=' . $this->get_suite_token($param), eyc_array_key($param, 'get_auth_info,get_auth_info'));
         if ($r["errcode"] == 0) {
             return $r;
         } else {
