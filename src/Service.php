92b4d5aa9029fda6cb7017b5a8085c88b9a2c890
bool <zhenbeigg@163.com>
2024-01-31 20:32:02 +0800

    修改:服务类

--- src/Service.php
+++ src/Service.php
@@ -3,7 +3,7 @@
  * @author: 布尔
  * @name: 钉钉Service类
  * @desc: 介绍
- * @LastEditTime: 2023-08-18 18:07:52
+ * @LastEditTime: 2024-01-30 16:56:43
  */
 
 namespace Eykj\Qyweixin;
@@ -43,33 +43,7 @@ class Service
      */
     public function get_suite_token(array $param): string
     {
-        $key = $param['corpid'] . '_' . $param['corp_product'] . '_' . $param['types'] . '_qyweixxin_suite_token';
-        if (!redis()->get($key)) {
-            /* 查询产品信息 */
-            $product_info = $this->Product->get_info(eyc_array_key($param, 'corp_product,types'));
-            if (!$product_info) {
-                error(13004);
-            }
-            /*查询suiteTicket*/
-            $filter['biz_type'] = 2;
-            $filter['biz_id'] = $product_info['qyweixin_suite_id'];
-            $filter['subscribe_id'] = $product_info['qyweixin_suite_id'] . '_0';
-            $biz_info = $this->BizDatum->get_info($filter);
-            if (!$biz_info) {
-                error(10002);
-            }
-            $data = eyc_array_key($product_info, 'suite_id|qyweixin_suite_id,suite_secret|qyweixin_suite_secret');
-            $data = eyc_array_insert($data, json_decode($biz_info['biz_data'], true), 'suite_ticket');
-            $r = $this->GuzzleHttp->post(env('QYWEIXIN_URL', 'https://qyapi.weixin.qq.com') . '/cgi-bin/service/get_suite_token', $data);
-            if ($r["errcode"] == 0) {
-                redis()->set($key, $r["suite_access_token"], $r['expires_in']);
-                return $r["suite_access_token"];
-            } else {
-                error(500, $r['errmsg']);
-            }
-        } else {
-            return redis()->get($key);
-        }
+        return $this->AuthInterface->get_suite_token('Qyweixin', $param);
     }
 
     /**
@@ -80,7 +54,7 @@ class Service
      */
     public function get_pre_auth_code(array $param): string
     {
-        $key = $param['corpid'] . '_' . $param['corp_product'] . '_' . $param['types'] . '_qyweixxin_pre_auth_code';
+        $key = $param['corp_product'] . '_' . $param['types'] . '_qyweixxin_pre_auth_code';
         if (!redis()->get($key)) {
             $r = $this->GuzzleHttp->get(env('QYWEIXIN_URL', 'https://qyapi.weixin.qq.com') . '/cgi-bin/service/get_pre_auth_code?suite_access_token=' . $this->get_suite_token($param));
             if ($r["errcode"] == 0) {
@@ -118,7 +92,7 @@ class Service
      */
     public function get_auth_info(array $param): array
     {
-        $r = $this->GuzzleHttp->get(env('QYWEIXIN_URL', 'https://qyapi.weixin.qq.com') . '/cgi-bin/service/get_auth_info?suite_access_token=' . $this->get_suite_token($param), eyc_array_key($param, 'get_auth_info,get_auth_info'));
+        $r = $this->GuzzleHttp->get(env('QYWEIXIN_URL', 'https://qyapi.weixin.qq.com') . '/cgi-bin/service/get_auth_info?suite_access_token=' . $this->get_suite_token($param), eyc_array_key($param, 'auth_corpid,permanent_code'));
         if ($r["errcode"] == 0) {
             return $r;
         } else {
