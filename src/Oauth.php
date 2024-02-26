43688abc5efc8ad2a839c5ee9b554b047cde7e56
bool <zhenbeigg@163.com>
2024-02-26 17:48:36 +0800

    增加:授权接口

---
+++ src/Oauth.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * @author: 布尔
+ * @name: 企业微信授权接口类
+ * @desc: 介绍
+ * @LastEditTime: 2023-08-21 16:45:44
+ */
+
+namespace Eykj\Qyweixin;
+
+use Eykj\Base\GuzzleHttp;
+use Eykj\Qyweixin\Service;
+use function Hyperf\Support\env;
+
+class Oauth
+{
+    protected ?GuzzleHttp $GuzzleHttp;
+
+    protected ?Service $Service;
+
+    // 通过设置参数为 nullable，表明该参数为一个可选参数
+    public function __construct(?GuzzleHttp $GuzzleHttp, ?Service $Service)
+    {
+        $this->GuzzleHttp = $GuzzleHttp;
+        $this->Service = $Service;
+    }
+    /**
+     * @author: 布尔
+     * @name: 构造企业oauth2链接
+     * @param array $param
+     * @return array
+     */
+    public function authorize(array $param): array
+    {
+        $r = $this->GuzzleHttp->get(env('QYWEIXIN_URL', '') . '/connect/oauth2/authorize?appid=' . $param['corpid'] . '&redirect_uri=' . urlencode($param['redirect_uri']) . '&response_type=code&scope=' . $param['scope'] . '&agentid=' . $param['agentid'] . '&state=' . $param['state'] . '#wechat_redirect');
+        if ($r['errcode'] != 0) {
+            error(500, $r['errmsg']);
+        }
+        return $r;
+    }
+}
