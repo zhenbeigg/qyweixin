709210a573a62b387b527f732c2478550d8eda98
bool <zhenbeigg@163.com>
2024-02-27 11:01:42 +0800

    修改:获取授权链接方法

--- src/Oauth.php
+++ src/Oauth.php
@@ -3,7 +3,7 @@
  * @author: 布尔
  * @name: 企业微信授权接口类
  * @desc: 介绍
- * @LastEditTime: 2023-08-21 16:45:44
+ * @LastEditTime: 2024-02-27 11:01:03
  */
 
 namespace Eykj\Qyweixin;
@@ -32,7 +32,7 @@ class Oauth
      */
     public function authorize(array $param): array
     {
-        $r = $this->GuzzleHttp->get(env('QYWEIXIN_URL', '') . '/connect/oauth2/authorize?appid=' . $param['corpid'] . '&redirect_uri=' . urlencode($param['redirect_uri']) . '&response_type=code&scope=' . $param['scope'] . '&agentid=' . $param['agentid'] . '&state=' . $param['state'] . '#wechat_redirect');
+        $r = $this->GuzzleHttp->get(env('QYWEIXIN_URL', '') . '/connect/oauth2/authorize?appid=' . $param['appid'] . '&redirect_uri=' . urlencode($param['redirect_uri']) . '&response_type=code&scope=' . $param['scope'] .(isset($param['agentid'])?'&agentid=' . $param['agentid']:'') . '&state=' . $param['state'] . '#wechat_redirect');
         if ($r['errcode'] != 0) {
             error(500, $r['errmsg']);
         }
