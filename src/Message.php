2f4945ff92f492ce04253770100a79eb109f42d2
bool <zhenbeigg@163.com>
2024-03-01 16:39:45 +0800

    修改：消息通知类

--- src/Message.php
+++ src/Message.php
@@ -3,13 +3,15 @@
  * @author: 布尔
  * @name: 消息推送
  * @desc: 介绍
- * @LastEditTime: 2023-08-22 10:33:00
+ * @LastEditTime: 2024-03-01 16:38:42
  */
+
 namespace Eykj\Qyweixin;
 
 use Eykj\Base\GuzzleHttp;
 use Eykj\Qyweixin\Service;
 use function Hyperf\Support\env;
+use Eykj\Base\JsonRpcInterface\AuthInterface;
 
 class Message
 {
@@ -17,11 +19,14 @@ class Message
 
     protected ?Service $Service;
 
+    protected ?AuthInterface $AuthInterface;
+
     // 通过设置参数为 nullable，表明该参数为一个可选参数
-    public function __construct(?GuzzleHttp $GuzzleHttp,?Service $Service)
+    public function __construct(?GuzzleHttp $GuzzleHttp, ?Service $Service, ?AuthInterface $AuthInterface)
     {
         $this->GuzzleHttp = $GuzzleHttp;
         $this->Service = $Service;
+        $this->AuthInterface = $AuthInterface;
     }
 
     /**
@@ -32,7 +37,11 @@ class Message
      */
     public function send(array $param): array
     {
-        $r = $this->GuzzleHttp->post(env('QYWEIXIN_URL', '') . '/cgi-bin/message/send?access_token='. $this->Service->get_access_token($param), eyc_array_key($param, 'touser,toparty,totag,agentid,msgtype,selected_ticket_list,text,image,voice,video,file,textcard,news,mpnews,markdown,miniprogram_notice,template_card,template_msg,enable_id_trans,only_unauth,safe,enable_duplicate_check,duplicate_check_interval'));
+        /* 查询授权信息 */
+        $filter = eyc_array_key($param, 'corpid,types,corp_product');
+        $auth_info = $this->AuthInterface->get_info('Dtalk', $filter);
+        $param = eyc_array_insert($param, $auth_info, 'agentid');
+        $r = $this->GuzzleHttp->post(env('QYWEIXIN_URL', '') . '/cgi-bin/message/send?access_token=' . $this->Service->get_access_token($param), eyc_array_key($param, 'touser,toparty,totag,agentid,msgtype,selected_ticket_list,text,image,voice,video,file,textcard,news,mpnews,markdown,miniprogram_notice,template_card,template_msg,enable_id_trans,only_unauth,safe,enable_duplicate_check,duplicate_check_interval'));
         if ($r['errcode'] != 0) {
             error(500, $r['errmsg']);
         }
@@ -47,7 +56,11 @@ class Message
      */
     public function update_template_card(array $param): array
     {
-        $r = $this->GuzzleHttp->post(env('QYWEIXIN_URL', '') . '/cgi-bin/message/update_template_card?access_token='. $this->Service->get_access_token($param), eyc_array_key($param, 'userids,partyids,tagids,atall,agentid,response_code,button'));
+        /* 查询授权信息 */
+        $filter = eyc_array_key($param, 'corpid,types,corp_product');
+        $auth_info = $this->AuthInterface->get_info('Dtalk', $filter);
+        $param = eyc_array_insert($param, $auth_info, 'agentid');
+        $r = $this->GuzzleHttp->post(env('QYWEIXIN_URL', '') . '/cgi-bin/message/update_template_card?access_token=' . $this->Service->get_access_token($param), eyc_array_key($param, 'userids,partyids,tagids,atall,agentid,response_code,button'));
         if ($r['errcode'] != 0) {
             error(500, $r['errmsg']);
         }
@@ -62,7 +75,7 @@ class Message
      */
     public function recall(array $param): array
     {
-        $r = $this->GuzzleHttp->post(env('QYWEIXIN_URL', '') . '/cgi-bin/message/recall?access_token='. $this->Service->get_access_token($param), eyc_array_key($param, 'msgid'));
+        $r = $this->GuzzleHttp->post(env('QYWEIXIN_URL', '') . '/cgi-bin/message/recall?access_token=' . $this->Service->get_access_token($param), eyc_array_key($param, 'msgid'));
         if ($r['errcode'] != 0) {
             error(500, $r['errmsg']);
         }
